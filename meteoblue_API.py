f52663b33e1df9065ece9808e5d5834a0d8d5ff2
redgren-grumbholdt <163171411+redgren-grumbholdt@users.noreply.github.com>
2024-03-13 22:58:40 +0000

    added meteoblue API

--- meteoblue_API.py
+++ meteoblue_API.py
@@ -1,8 +1,11 @@
 import datetime as dt
 import requests
+import os
+
+print(os.environ)
 
 BASE_URL = "https://my.meteoblue.com/packages/basic-1h_basic-day_clouds-3h_clouds-day_trendpro-day?"
-METEOBLUE_API_KEY = secrets.METEOBLUE_API_KEY
+METEOBLUE_API_KEY = os.environ['METEOBLUE_API_KEY']
 LOCATION = "lat=62.920&lon=-151.070"
 ELEVATION = ""
 
