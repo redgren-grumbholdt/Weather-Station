09c2e07fcf94574d53005bed66f415200e5c5de4
redgren-grumbholdt <163171411+redgren-grumbholdt@users.noreply.github.com>
2024-03-14 00:54:44 +0000

    another one

--- meteoblue_API.py
+++ meteoblue_API.py
@@ -2,20 +2,31 @@ import datetime as dt
 import requests
 import json
 from dotenv import load_dotenv
+import os
 
 BASE_URL = "https://my.meteoblue.com/packages/basic-3h_basic-day_clouds-3h_trend-day"
 LOCATION = "lat=62.920&lon=-151.070"
 ELEVATION = ""
 
-url = BASE_URL + "?apikey=" + METEOBLUE_API_KEY + "&" + LOCATION + "&"
-if ELEVATION != "":
-    url += "&"
-url += "format=json"
 
-print(url)
-response = requests.get(url).json()
+def configure():
+    load_dotenv()
 
-with open("recent_forecast.json", "w") as file:
-    json.dump(response, file)
 
-print(response)
+def main():
+    configure()
+
+    url = BASE_URL + "?apikey=" + os.getenv('METEOBLUE_API_KEY') + "&" + LOCATION + "&"
+    if ELEVATION != "":
+        url += "&"
+    url += "format=json"
+
+    print(url)
+    response = requests.get(url).json()
+
+    with open("recent_forecast.json", "w") as file:
+        json.dump(response, file)
+
+    print(response)
+
+main()
